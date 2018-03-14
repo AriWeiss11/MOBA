b599a80571aae5d47b39ccef2bb46e51f0f00901
AriWeiss11 <33332173+AriWeiss11@users.noreply.github.com>
2018-03-14 12:04:31 -0500

    Main Map
    Has issues with movement but will be fixed

---
+++ Assets/RecivedMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecivedMovement : MonoBehaviour {
+
+
+Vector3 newpositon; 
+public float speed; 
+public float walkrange;
+
+public GameObject graphics;
+
+	// Use this for initialization
+	void Start () {
+		
+		newpositon = this.transform.position 
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if the (Vector3.Distance (newpositon, this.transform.position) > walkrange)
+		        this.transform.position = Vector3.MoveTowards(this.transform.position, newpositon, speed * time.deltaTime);
+		        Quaternion transRot = Quaternion.LookRotation(newpositon - this.transform.position, Vector3.up);
+		        graphics.transform.rotation = Quaternion.Slerp(transRot, graphics.transform.rotation, 0.2f);
+	}
+}
+
+[PunRPC]
+public void RecivedMove(Vector3 movePos)
+    newpositon = movePos;
+}
+
+
+}
+
---
+++ Assets/SendInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendInfo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		bool RMB = input.GetMouseButtonDown (1);
+
+		if (RMB)
+
+		    RaycastHit hit 
+		    Ray ray = Camera.main.ScreenPointToRay(input.mousePosition);
+
+		    if (Physics.Raycast(ray, out hit) && hit.transform.tag == "New Terrain") {
+		    	this.GetComponent<PhotonView>() .RPC("RecivedMove", PhotonTargets.ALL, hit.point);
+		
+	}
+}
